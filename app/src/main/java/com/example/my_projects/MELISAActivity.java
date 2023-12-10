cfa021522373d6140f566b32caf6fcdcde78b379
Olesua11 <Lesavajdeman@gmail.com>
2023-12-10 20:39:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/my_projects/MELISAActivity.java
@@ -0,0 +1,14 @@
+package com.example.my_projects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MELISAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_melisaactivity);
+    }
+}
